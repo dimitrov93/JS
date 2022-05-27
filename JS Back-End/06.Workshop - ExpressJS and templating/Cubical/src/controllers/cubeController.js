67f6a7886fd7ce90a7b27e0ca109bd0772b5cfc0
dimitrov93 <ts.dimitrov93@gmail.com>
2022-05-28 02:50:23 +0300

    render static details page

--- JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/controllers/cubeController.js
+++ JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/controllers/cubeController.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
-const fs = require('fs/promises');
-const cubes = require('../db.json');
-const path = require('path');
 
+const cubeService = require('../services/cubeServices')
 
 router.get('/create', (req,res) => {
     res.render('create')
 });
 
-router.post('/create', (req,res) => {
+router.post('/create', async (req,res) => {
     const cube = req.body;
     // validate
     if (cube.name.length < 2) {
         return res.status(400).send('Invalid request');
     }
     // save data
-    cubes.push(cube);
-    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {encoding:"utf-8"})
-        .then(() => {
-            res.redirect('/')
-        })
-        .catch(err => {
-            res.status(400).send(err)
-        });
-    // redirect to page
+    try {
+        await cubeService.save(cube)
+        res.redirect('/')
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+});
+
+router.get('/details/:id' ,(req,res) => {
+    res.render('details')
 });
 
 module.exports = router

dd3f2f90d335f7d39d55c6cdd8e2569746aec591
dimitrov93 <ts.dimitrov93@gmail.com>
2022-05-28 02:59:36 +0300

    add increamented id to create cubes

--- JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/services/cubeServices.js
+++ JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/services/cubeServices.js
@@ -6,7 +6,7 @@ const cubes = require('../db.json');
 exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
-    cubes.push(cube);
+    cubes.push({id: cubes[cubes.length - 1].id + 1, ...cube});
 
     let textData = JSON.stringify(cubes, '', 4);
     return fs.writeFile(path.resolve('src', 'db.json'), textData , {encoding:"utf-8"})

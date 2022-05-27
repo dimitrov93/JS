559ee1fdc5d6318b1352307e4ce10578b4bdeae5
dimitrov93 <ts.dimitrov93@gmail.com>
2022-05-27 12:50:38 +0300

    add about page and fix navigation

---
+++ JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/controllers/cubeController.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+router.get('/create', (req,res) => {
+    res.render('create')
+})
+module.exports = router
--- JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/routes.js
+++ JS Back-End/06.Workshop - ExpressJS and templating/Cubical/src/routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('./controllers/homeController.js');
+const cubeController = require('./controllers/cubeController.js')
 
-router.get('/', homeController.index)
+router.get('/', homeController.index);
+router.get('/about', homeController.about);
+router.use('/cube', cubeController);
+
 module.exports = router;
 // module.exports = (app) => {
 //     app.get('/', homeController.index)

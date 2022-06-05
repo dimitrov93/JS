24a42a70067252aef0d00d27a2a1d89c175c17c6
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-05 17:33:01 +0300

    create post accessory page

--- JS Back-End/08.Workshop - NoSQL and MongoDB/cubical/src/controllers/accesoryController.js
+++ JS Back-End/08.Workshop - NoSQL and MongoDB/cubical/src/controllers/accesoryController.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
+
 router.get('/create', (req,res) => {
     res.render('accessory/create')
-})
+});
+
+router.post('/create', (req,res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 module.exports = router;

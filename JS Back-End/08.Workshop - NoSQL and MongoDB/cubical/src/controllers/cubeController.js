f7a987956733186fc83813f9a0f23c3ccfa7169d
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-05 18:13:11 +0300

    adding dynamic rendering for cube accessories

--- JS Back-End/08.Workshop - NoSQL and MongoDB/cubical/src/controllers/cubeController.js
+++ JS Back-End/08.Workshop - NoSQL and MongoDB/cubical/src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const cubeService = require('../services/cubeServices')
+const accessoryService = require('../services/accessoryService')
 
 router.get('/create', (req,res) => {
     res.render('create')
@@ -30,8 +31,9 @@ router.get('/details/:id' , async (req,res) => {
 
 router.get('/:cubeId/attach', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId);
+    const accessories = await accessoryService.getAll().lean();
 
-    res.render('accessory/attach', {cube})
+    res.render('accessory/attach', {cube, accessories})
 })
 
 module.exports = router

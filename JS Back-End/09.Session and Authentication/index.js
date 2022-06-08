6e20b44179ece8cc4c048a72e63510369cadee09
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-08 13:52:22 +0300

    authentication

--- JS Back-End/09.Session and Authentication/index.js
+++ JS Back-End/09.Session and Authentication/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const app = express();
 
+const userSessions = {};
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 app.use(express.urlencoded({extended: false}))
 app.use('/static', express.static('public'))
@@ -18,12 +23,36 @@ app.get('/register', function(req, res) {
     res.render('register');
 });
 
-app.post('/register', (req,res) => {
-    console.log(req.body);
+app.post('/register', async (req,res) => {
+    const { email, password } = req.body
+    if (userSessions[email]) {
+        res.status(400).send('User already exists')
+    }
+
+    const hash = await bcrypt.hash(password,saltRounds);
+    userSessions[email] = {
+        email,
+        password: hash
+    }
+
+    res.redirect('/login')
 })
 
-app.get('/login', function(req, res) {
+app.get('/login', async function(req, res) {
     res.render('login');
+});
+
+app.post('/login',  async function(req, res) {
+    const {email,password} = req.body;
+
+    const isAuthenticated = await bcrypt.compare(password, userSessions[email].password);
+
+    if (isAuthenticated) {
+        res.redirect('/')
+    } else {
+        res.status(401).send('Wrong')
+    }
+
 });
 
 app.listen(5000, () => console.log("Server is running on 5000"))

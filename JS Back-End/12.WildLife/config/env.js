8f4fd24cb7f7771ad1cb8c4b118bd82e604bb473
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-22 00:57:36 +0300

    db added

--- JS Back-End/12.WildLife/config/env.js
+++ JS Back-End/12.WildLife/config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3000;
-exports.DB_QUERY_STRING = 'mongodb://localhost:27017/realEstate';
+exports.DB_QUERY_STRING = 'mongodb://localhost:27017/wildLife';
 exports.SALT_ROUNDS = 10;
 exports.SECRET = '23eb16ae42981b643b18d7b9bc848ddf'
--- JS Back-End/12.WildLife/index.js
+++ JS Back-End/12.WildLife/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const {PORT} = require('./config/env')
 const routes = require('./router');
+const {startDB} = require('./config/initDB')
 const app = express();
 app.engine('hbs', hbs.engine({
     extname: 'hbs'
@@ -12,4 +13,7 @@ app.set('view engine', 'hbs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(routes);
+
+startDB();
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

327355b483ef5b65448b09b595fa23fdbcd682e6
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-22 12:21:59 +0300

    fixed reg/log/login

--- JS Back-End/12.WildLife/index.js
+++ JS Back-End/12.WildLife/index.js
@@ -4,6 +4,12 @@ const {PORT} = require('./config/env')
 const routes = require('./router');
 const {startDB} = require('./config/initDB')
 const app = express();
+const {auth} = require('./middlewares/auth');
+const cookieParser = require('cookie-parser');
+
+
+
+
 app.engine('hbs', hbs.engine({
     extname: 'hbs'
 }));
@@ -12,6 +18,9 @@ app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
+
+app.use(cookieParser());
+app.use(auth)
 app.use(routes);
 
 startDB();

3c1be62645b743925fe95fbf4e09473511ad09b2
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-23 17:01:39 +0300

    create page created

--- JS Back-End/14.SharedTips/controllers/tripController.js
+++ JS Back-End/14.SharedTips/controllers/tripController.js
@@ -1,16 +1,32 @@
 const router = require('express').Router();
+const { isAuth } = require('../middlewares/authMiddleware');
+const tripService = require('../services/tripService');
+const {getErrorMsg} = require('../utils/errorHelpers');
 
-router.get('/create', (req,res) => {
+
+router.get('/create', isAuth, (req,res) => {
     res.render('trips/create')
 });
 
-router.get('/details', (req,res) => {
+router.get('/details', isAuth, (req,res) => {
     res.render('trips/details')
 });
 
-router.get('/shared', (req,res) => {
+router.get('/shared', isAuth, (req,res) => {
     res.render('trips/shared')
 });
 
 
+// ------------ POST ----------------- ///
+
+router.post('/create', isAuth, async (req,res) => {
+    const tripData = {...req.body, creator: req.user._id}
+
+    try {
+        await tripService.create(tripData);
+        res.redirect('/trip/shared')
+    } catch (error) {
+        res.render('trips/create', {...req.body, error: getErrorMsg(error)})
+    }
+});
 module.exports = router;

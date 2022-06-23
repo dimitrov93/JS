a717c08420661b7ed57066d018ff1d046484e0c6
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-23 12:40:37 +0300

    profile

--- JS Back-End/13.BookingUni/controllers/homeController.js
+++ JS Back-End/13.BookingUni/controllers/homeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const hotelService = require('../services/hotelService');
+const userService = require('../services/userService');
 const {getErrorMsg} = require('../utils/errorHelpers');
 
 
@@ -12,6 +13,13 @@ router.get('/', async (req,res) => {
     } catch (error) {
         res.render('home', {error: getErrorMsg(error)} )
     }
+});
+
+router.get('/profile', async (req,res) => {
+        const theUser = await userService.FindById(req.user._id).lean();
+        const userHotels = await userService.UserHotels(req.user._id).lean();
+        console.log(userHotels);
+        res.render('home/profile', {...theUser, ...userHotels})
 });
 
 module.exports = router;
--- JS Back-End/13.BookingUni/services/userService.js
+++ JS Back-End/13.BookingUni/services/userService.js
@@ -1,3 +1,4 @@
 const User = require('../models/User');
 
 exports.FindById = (userId) => User.findById(userId)
+exports.UserHotels = (userId) => User.findById(userId).populate('booked', 'name')

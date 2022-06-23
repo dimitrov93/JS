14df14cea252bd58e1a51f50a663c17f836ac4e4
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-23 12:59:24 +0300

    initial setup

---
+++ JS Back-End/14.SharedTips/controllers/homeController.js
@@ -0,0 +1,9 @@
+const { isAuth } = require('../middlewares/authMiddleware');
+
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+    res.render('home/home')
+});
+
+module.exports = router;

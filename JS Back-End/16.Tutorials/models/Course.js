760ebf60d0f985dfc3ce4077c94d850519bf8c15
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-24 13:44:44 +0300

    mongoose timestamp

--- JS Back-End/16.Tutorials/models/Course.js
+++ JS Back-End/16.Tutorials/models/Course.js
@@ -2,37 +2,17 @@ const mongoose = require('mongoose');
 
 
 const courseSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true,
-        maxLength: 50
+    title: {type: String,required: [true, 'Username is required'],unique: true,maxLength: 50},
+    description: {type: String,required: true,},
+    image: {type: String,required: true,},
+    duration: {type: String,required: true,},
+    usersEnrolled: [{type: mongoose.Types.ObjectId,ref: 'User'}],
+    author: {type: mongoose.Types.ObjectId,ref: 'User'}
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    duration: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: String,
-        timestamps: true
-    },
-    usersEnrolled: [{
-        type: mongoose.Types.ObjectId,
-        ref: 'User'
-    }],
-    author: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User'
+    {
+     timestamps: true
     }
-});
+);
 
 
 const Course = mongoose.model('Course', courseSchema);

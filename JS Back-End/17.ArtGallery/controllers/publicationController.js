e40912449c59e09cc886282932459ca931b90628
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-24 18:32:16 +0300

    edit/delete

--- JS Back-End/17.ArtGallery/controllers/publicationController.js
+++ JS Back-End/17.ArtGallery/controllers/publicationController.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const publicationService = require('../services/publicationService');
 const { getErrorMsg } = require('../utils/errorHelpers');
 
-router.get('/gallery', isAuth, async (req,res) => {
+router.get('/gallery', async (req,res) => {
     const galleries = await publicationService.getAll().lean();
     res.render('publications/gallery', {title: 'Gallery Page', galleries})
 });
@@ -21,6 +21,31 @@ router.post('/create', isAuth, async (req,res) => {
         res.redirect('/publications/gallery')
     } catch (error) {
         res.render('publications/create', {...req.body, error: getErrorMsg(error)})
+    }
+});
+
+router.get('/:postId/details',  async (req,res) => {
+    const currentPost = await publicationService.getOneDetailed(req.params.postId).lean();
+    const isOwner = currentPost.author._id == req.user?._id;
+    res.render('publications/details', {...currentPost, isOwner})
+});
+
+router.get('/:postId/delete',  async (req,res) => {
+    await publicationService.delete(req.params.postId);
+    res.redirect('/publications/gallery')
+});
+
+router.get('/:postId/edit',  async (req,res) => {
+    const currentPost = await publicationService.getOne(req.params.postId).lean();
+    res.render('publications/edit', {...currentPost})
+});
+
+router.post('/:postId/edit',  async (req,res) => {
+    try {
+        await publicationService.update(req.params.postId, {...req.body});
+        res.redirect(`/publications/${req.params.postId}/details`)
+    } catch (error) {
+        res.render(`/publications/${req.params.postId}/edit`, {...req.body, error: getErrorMsg(error)})
     }
 });
 

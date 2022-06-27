bfa5b2a918bd67dde3cac6f9e289ee371d5848f8
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-27 13:14:36 +0300

    edit

--- JS Back-End/RestApi/server/src/controllers/furniture.js
+++ JS Back-End/RestApi/server/src/controllers/furniture.js
@@ -26,4 +26,40 @@ router.post('/', async (req,res) => {
 
 });
 
+router.get('/:id', async (req,res) => {
+    const id = req.params.id;
+    const item = await api.getById(id);
+    if (item) {
+        res.json(item)
+    } else {
+        res.status(404).json({message: `Item ${id} not found`})
+    }
+});
+
+router.put('/:id', async (req,res) => {
+    const id = req.params.id;
+    
+    const item = {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        description: req.body.description,
+        price: req.body.price,
+        img: req.body.img,
+        material: req.body.material
+    };
+
+    try {
+        const result = await api.updateById(id, item)
+        res.json(result);
+    } catch (err) {
+        if (err._notFound) {
+            res.status(404).json({message: `Item ${id} not found`})
+        } else {
+            console.log(err);
+            res.status(400).json({message: 'Request Error'})
+        }
+    }
+});
+
 module.exports = router
--- JS Back-End/RestApi/server/src/services/furniture.js
+++ JS Back-End/RestApi/server/src/services/furniture.js
@@ -19,7 +19,34 @@ async function create(item) {
     return result;
 }
 
+async function getById(id) {
+    return Item.findById(id);
+}
+
+async function updateById(id, item) {
+    const existing = await Item.findById(id);
+
+    if (existing) {
+        existing.make = item.make;
+        existing.model = item.model;
+        existing.year = item.year;
+        existing.description = item.description;
+        existing.price = item.price;
+        existing.img = item.img;
+        existing.material = item.material
+    } else {
+        const error = new Error('Not Found')
+        error._notFound = true;
+        throw error;
+    }
+
+    await existing.save();
+    return existing;
+}
+
 module.exports = {
     getAll,
-    create
+    create,
+    getById,
+    updateById
 }

5680103be4f8a9f0ee50e3f5918b8d2a2101c470
dimitrov93 <ts.dimitrov93@gmail.com>
2022-07-08 12:08:31 +0300

    workshop initial setup

---
+++ ReactJS/04.Workshop/client/src/App.js
@@ -0,0 +1,38 @@
+import { Footer } from "./components/common/Footer";
+import { Header } from "./components/common/Header";
+import './App.css';
+import { Search } from "./components/search/Search";
+import { UserList } from "./components/user-list/UserSection";
+import {useEffect, useState} from 'react';
+
+const baseUrl= 'http://localhost:3005/api';
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${baseUrl}/users`)
+      .then(res => res.json())
+      .then(result => {
+        setUsers(result.users)
+      })
+  }, [])
+
+  console.log(users);
+  return (
+    <div className="App">
+        <Header />
+
+        <main className="main">
+        <section className="card users-container">
+          <Search />
+          <UserList users={users} />
+        </section>
+        </main>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default App;

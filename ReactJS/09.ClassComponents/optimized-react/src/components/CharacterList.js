c8c21bec680b80ebeb759b9860d085e89ee38642
dimitrov93 <ts.dimitrov93@gmail.com>
2022-08-04 13:22:38 +0300

    useMemo

--- ReactJS/09.ClassComponents/optimized-react/src/components/CharacterList.js
+++ ReactJS/09.ClassComponents/optimized-react/src/components/CharacterList.js
@@ -1,5 +1,13 @@
 import  CharItem  from "./CharItem";
-import {useState, useCallback} from 'react';
+import {useState, useCallback, useMemo} from 'react';
+
+function wait(ms) {
+  var start = Date.now(),
+      now = start;
+  while (now - start < ms) {
+    now = Date.now();
+  }
+}
 
 const CharacterList = ({ characters }) => {
   const [favorite, setFavorite] = useState('');
@@ -7,9 +15,17 @@ const CharacterList = ({ characters }) => {
   const selectFavorite = useCallback((name) => {
     setFavorite(name);
   }, []);
+
+  const meaningOfLife = useMemo(() => {
+    wait(500);
+    return 42
+  }, [])
+
+
   return (
     <>
       <h2>{favorite || 'Select Favorite'}</h2>
+      <h3>{meaningOfLife}</h3>
       <ul>
         {characters.map((x) => (
           <CharItem key={x.name} name={x.name} onClick={selectFavorite} />

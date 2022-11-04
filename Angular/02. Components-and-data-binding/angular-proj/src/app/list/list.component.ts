080aa66d7743f9e995a54642735fab240a3a9e5c
dimitrov93 <ts.dimitrov93@gmail.com>
2022-11-04 10:38:13 +0200

    binding data

---
+++ Angular/02. Components-and-data-binding/angular-proj/src/app/list/list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  // interpolation: ['[', ']'],
+
+})
+export class ListComponent {
+
+  users = [
+    {
+      firstName: 'Ivan',
+      lastName: 'Petrov'
+    },
+    {
+      firstName: 'Petar',
+      lastName: 'Ivanov'
+    }
+  ];
+  showLastName = true;
+
+  
+  handleClickEvent(event: MouseEvent) {
+    console.log(event);
+    
+    this.showLastName = !this.showLastName
+    
+  }
+  constructor() { 
+
+  }
+
+}

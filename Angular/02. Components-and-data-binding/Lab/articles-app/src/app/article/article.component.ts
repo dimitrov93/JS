2dc4c03edf42cfd60b4d33eb6ec278b2ed5ed184
dimitrov93 <ts.dimitrov93@gmail.com>
2022-11-07 10:46:45 +0200

    Lab - components

---
+++ Angular/02. Components-and-data-binding/Lab/articles-app/src/app/article/article.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent  {
+
+  private symbols: number = 250;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = "Show Image";
+
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+   }
+
+   readMore(): void {
+    this.articleDescLen += this.symbols;
+    if (this.articleDescLen > this.articleDesc.length) {
+      this.showHideBtn = !this.showHideBtn;
+      this.showReadMoreBtn = !this.showReadMoreBtn;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen)
+    }
+   };
+
+   toggleImage(): void {
+    this.imageIsShown = !this.imageIsShown;
+    this.imageButtonTitle = this.imageButtonTitle === "Show Image" ? "Hide Image" : "Show Image";
+   };
+
+
+   hideDesc(): void {
+    this.descToShow = "";
+    this.articleDescLen = 0;
+    this.showHideBtn = false;
+    this.showReadMoreBtn = true;
+   }
+
+
+
+
+}
---
+++ Angular/02. Components-and-data-binding/Lab/articles-app/src/app/models/article.model.ts
@@ -0,0 +1,8 @@
+export class Article {
+  constructor(
+    public title: string,
+    public descritpion: string,
+    public author: string,
+    public imageUrl: string
+  ) {}
+}

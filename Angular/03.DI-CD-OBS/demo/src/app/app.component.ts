f4c60e18414c067943fe5460b30f598c74aded8a
dimitrov93 <ts.dimitrov93@gmail.com>
2022-11-08 09:45:54 +0200

    monkey patching

---
+++ Angular/03.DI-CD-OBS/demo/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  counter = 0;
+  constructor() {
+    setInterval(() => {
+      this.counter ++;
+    },3000)
+  }
+}
---
+++ Angular/03.DI-CD-OBS/demo/src/main.ts
@@ -0,0 +1,20 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  // money patching
+  const _setInterval = window.setInterval;
+  (window as any).setInterval = function(...args: any[]): number {
+    console.log('setInterval was called', args);
+    return _setInterval.apply(this, args as any) as unknown as number;
+    
+  }

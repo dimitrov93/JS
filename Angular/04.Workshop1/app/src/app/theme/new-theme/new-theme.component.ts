c086ac2fa35d78dc84e53f3d6a931f91161135a6
dimitrov93 <ts.dimitrov93@gmail.com>
2022-11-27 14:30:47 +0200

    new theme

--- Angular/04.Workshop1/app/src/app/theme/new-theme/new-theme.component.ts
+++ Angular/04.Workshop1/app/src/app/theme/new-theme/new-theme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-theme',
@@ -12,4 +13,11 @@ export class NewThemeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  newThemeHandler(form: NgForm): void {
+    if (form.invalid) {
+      return
+    }
+    console.log(form.value);
+
+  }
 }

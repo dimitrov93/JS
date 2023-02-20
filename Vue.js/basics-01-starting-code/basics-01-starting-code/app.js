99c3ec561e690b5b6636a16967b1270eaf4c855d
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-20 12:29:45 +0200

    vue - getting started, interpolation + bind

---
+++ Vue.js/basics-01-starting-code/basics-01-starting-code/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            goal:"Finish the course",
+            vueLink: 'https://vuejs.org/'
+        };
+    }
+});
+app.mount('#body');

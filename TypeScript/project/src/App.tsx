aa1e4af0265435569c64f23ee4a312fdf9090ddd
dimitrov93 <ts.dimitrov93@gmail.com>
2023-05-05 11:29:39 +0300

    footer + header

--- TypeScript/project/src/App.tsx
+++ TypeScript/project/src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { useState } from "react"
+import Cart from "./components/Cart"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Products from "./components/Products"
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+function App() {
+const [viewCart, setViewCart] = useState<boolean>(false)
+
+const pageContent = viewCart ? <Cart /> : <Products />
+
+const content = (
+  <>
+    <Header viewCart = {viewCart} setViewCart={setViewCart} />
+    {pageContent}
+    <Footer viewCart={viewCart} />
+  </>
+)
+  return content
 }
 
 export default App

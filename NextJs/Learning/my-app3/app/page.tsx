4fc337644b84181dd0368086699b6ba40399f833
dimitrov93 <ts.dimitrov93@gmail.com>
2023-05-26 11:29:49 +0300

    blog website init

---
+++ NextJs/Learning/my-app3/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+    <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+      Hello and Welcome 👋&nbsp;
+      <span className="whitespace-nowrap">
+        I&apos;m <span className="font-bold">Ceko</span>.
+      </span>
+    </p>
+  </main>
+
+  );
+}

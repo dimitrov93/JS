4d00c2ea3b14799c537bf040736797ba4c310876
dimitrov93 <ts.dimitrov93@gmail.com>
2023-05-29 11:16:24 +0300

    blog posts

--- NextJs/Learning/my-app3/app/components/Navbar.tsx
+++ NextJs/Learning/my-app3/app/components/Navbar.tsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from "react-icons/fa";
 
 export default function Navbar() {
-  return (
-    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-      <div className="prose prose-xl mx-auto flex justify-between flex-col sm-flex-row">
-        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-          <Link
-            href="/"
-            className="text-white/90 no-underline hover:text-white"
-          >
-            Tsvetomir Dimitrov
-          </Link>
-        </h1>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+                    <Link href="/" className="text-white/90 no-underline hover:text-white">Tsvetomir Dimitrov</Link>
+                </h1>
+                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                    <Link className="text-white/90 hover:text-white" href="/">
+                        <FaYoutube />
+                    </Link>
+                    <Link className="text-white/90 hover:text-white" href="/">
+                        <FaLaptop />
+                    </Link>
+                    <Link className="text-white/90 hover:text-white" href="https://github.com/dimitrov93">
+                        <FaGithub />
+                    </Link>
+                    <Link className="text-white/90 hover:text-white" href="/">
+                        <FaTwitter />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
--- NextJs/Learning/my-app3/app/page.tsx
+++ NextJs/Learning/my-app3/app/page.tsx
@@ -1,3 +1,5 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
@@ -7,7 +9,8 @@ export default function Home() {
         I&apos;m <span className="font-bold">Ceko</span>.
       </span>
     </p>
-  </main>
+    <Posts />
 
+  </main>
   );
 }

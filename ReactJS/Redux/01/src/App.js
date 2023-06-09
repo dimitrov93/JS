d10581e8dd8f229e52cc4116489f5b082dfbc534
dimitrov93 <ts.dimitrov93@gmail.com>
2023-06-09 11:05:32 +0300

    Redux intro

---
+++ ReactJS/Redux/01/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;

df92a4a4846b2b07a5185d5f2438f59c5ffd3f69
dimitrov93 <ts.dimitrov93@gmail.com>
2023-06-20 10:42:31 +0300

    RTK Querry

---
+++ ReactJS/Redux/05.RTK Querry/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "./features/api/apiSlice";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
+);

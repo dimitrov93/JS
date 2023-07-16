38f77043655e4a66c1e392f78ab0919f1373160a
dimitrov93 <ts.dimitrov93@gmail.com>
2023-07-16 13:56:41 +0300

    assignment + provide/eject pattern

---
+++ Vue.js/08.component-communication/assignment/src/App.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div>
+    <h1>hi</h1>
+    <active-user
+      v-for="user in users"
+      :key="user.name"
+      :name="user.name"
+      :age="user.age"
+    ></active-user>
+    <user-data
+    @send-data="getUserData"
+    ></user-data>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { name: "Tsvetomir dimitrov", age: "26" },
+        { name: "Dimitrov Tsvetomir", age: "62" },
+      ],
+    };
+  },
+  methods: {
+    getUserData(name,age) {
+        this.users.push({name,age})
+    }
+  }
+};
+</script>
+<style lang=""></style>
---
+++ Vue.js/08.component-communication/assignment/src/components/ActiveUser.vue
@@ -0,0 +1,12 @@
+<template lang="">
+  <div>
+    <h2>Username: {{ name }}</h2>
+    <h3>Age: {{ age }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  props: ["name", "age"],
+};
+</script>
+<style lang=""></style>

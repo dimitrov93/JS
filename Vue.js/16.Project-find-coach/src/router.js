3ce980d6f52014f2d5246bf31e0fe185c04ef599
dimitrov93 <ts.dimitrov93@gmail.com>
2023-07-23 20:12:48 +0300

    lazy loading

--- Vue.js/16.Project-find-coach/src/router.js
+++ Vue.js/16.Project-find-coach/src/router.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Coachdetails from "./pages/coaches/CoachDetails.vue";
 import CoachesList from "./pages/coaches/CoachesList.vue";
-import CoachRegister from "./pages/coaches/CoachRegister.vue";
-import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestsReceived from "./pages/requests/RequestsReceived.vue";
 import NotFound from "./pages/NotFound.vue";
-import UserAuth from "./pages/auth/UserAuth.vue";
 import store from "./store/store";
+
+const Coachdetails = () => import("./pages/coaches/CoachDetails.vue");
+const CoachRegister = () => import("./pages/coaches/CoachRegister.vue");
+const ContactCoach = () => import("./pages/requests/ContactCoach.vue");
+const RequestsReceived = () => import("./pages/requests/RequestsReceived.vue");
+const UserAuth = () => import("./pages/auth/UserAuth.vue");
 
 const router = createRouter({
   history: createWebHistory(),

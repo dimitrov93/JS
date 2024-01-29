06fab5b3ed8d17dce4bdea116365bbf947250370
dimitrov93 <ts.dimitrov93@gmail.com>
2024-01-29 21:52:35 +0200

    adding env

--- NextJs/myNextPortfolio/next.config.mjs
+++ NextJs/myNextPortfolio/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    env: {
+      mongodb_username: "dimitrov93",
+      mongodb_password: "dimitrov93",
+    },
+  },
+};
 
 export default nextConfig;

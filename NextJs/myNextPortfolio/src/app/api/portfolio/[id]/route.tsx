7590b537b5955b34cc4827ee634fde650affe02a
dimitrov93 <ts.dimitrov93@gmail.com>
2024-02-12 10:35:46 +0200

    route handlers for portfolio created

---
+++ NextJs/myNextPortfolio/src/app/api/portfolio/[id]/route.tsx
@@ -0,0 +1,48 @@
+import { connectDb } from "../../../../../db";
+import Portfolio from "../../../../../models/Portfolio";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const portfolioId = params.id;
+
+  try {
+    await connectDb();
+    const res = await Portfolio.findById(portfolioId);
+    return Response.json(res, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const data = await req.json();
+  const portfolioId = params.id;
+
+  try {
+    await connectDb();
+    await Portfolio.updateOne({ _id: portfolioId }, { $set: data });
+    return Response.json({ message: "Successfully Edited!" }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const portfolioId = params.id;
+
+  try {
+    await connectDb();
+    await Portfolio.findByIdAndDelete(portfolioId);
+    return Response.json({ message: "Successfully deleted!" }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
--- NextJs/myNextPortfolio/src/app/api/portfolio/route.tsx
+++ NextJs/myNextPortfolio/src/app/api/portfolio/route.tsx
@@ -4,8 +4,20 @@ import Portfolio from "../../../../models/Portfolio";
 export async function GET(req: Request, res: Response) {
   try {
     await connectDb();
-    const projects = await Portfolio.find()    
-    return Response.json(projects, {status: 200})
+    const projects = await Portfolio.find();
+    return Response.json(projects, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request, res: Response) {
+  const data = await req.json();
+
+  try {
+    await connectDb();
+    await Portfolio.create(data);
+    return Response.json({ message: "Successfully created!" }, { status: 200 });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }

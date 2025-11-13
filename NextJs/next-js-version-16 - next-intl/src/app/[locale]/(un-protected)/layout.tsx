af478473838b8009df652d20d775f6d41c19a0c6
Tsvetomir Dimitrov <ts.dimitrov93@gmail.com>
2025-11-13 22:37:10 +0200

    re-arragenet layouts

--- NextJs/next-js-version-16 - next-intl/src/app/[locale]/(un-protected)/layout.tsx
+++ NextJs/next-js-version-16 - next-intl/src/app/[locale]/(un-protected)/layout.tsx
@@ -1,8 +1,30 @@
-export default function UnprotectedLayout({ children }: { children: React.ReactNode }) {
+import { routing } from '@/src/i18n/routing';
+import { hasLocale } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
+// export default function UnprotectedLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <>
+//       <h1>unprotected layout</h1>
+//       {children}
+//     </>
+//   );
+// }
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function RootLayout({ children }: Props) {
   return (
-    <>
-      <h1>unprotected layout</h1>
-      {children}
-    </>
+    <html lang={'en'}>
+      <head></head>
+      <body>
+        <h1>unprotected layout</h1>
+        {children}
+      </body>
+    </html>
   );
 }
